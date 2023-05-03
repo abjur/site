ec3d07c80300ff84721e3436911e397ce88038d0
rmhirota <re.hirota@gmail.com>
2023-05-03 15:03:58 -0300

    rm magrittr, fix img

--- static/img/pesquisas/resize_images.R
+++ static/img/pesquisas/resize_images.R
@@ -1,5 +1,4 @@
-library(magrittr)
-"static/img/pesquisas/camaras.png" %>%
+"static/img/pesquisas/camaras.png" |>
   magick::image_read()
 
 
@@ -18,19 +17,19 @@ resize_image <- function(x) {
   h <- magick::image_info(img)$height
   w <- magick::image_info(img)$width
   border <- round((w / ar - h) / 2)
-  img %>%
+  img |>
     magick::image_border(
       "white",
       paste0("x", border)
-    ) %>%
+    ) |>
     magick::image_write(x)
 }
 
-fs::dir_ls("static/img/pesquisas", regexp = "jpg|png|svg") %>%
+fs::dir_ls("static/img/pesquisas", regexp = "jpg|png|svg") |>
   purrr::walk(resize_image)
 
 
-images <- fs::dir_ls("static/img/pesquisas", regexp = "jpg|png|svg") %>%
+images <- fs::dir_ls("static/img/pesquisas", regexp = "jpg|png|svg") |>
   purrr::map(magick::image_read)
 
 images[[5]]
